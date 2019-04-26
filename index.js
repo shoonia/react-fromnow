917808bf712c58ce4025b9f4f1bcb62ab445673a
Alexander Zaytsev <zaytsev126@gmail.com>
2019-04-27 00:54:47 +0300

    Fix export

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import Fromnow from './Fromnow'
+
 export { default as en } from './en';
 export { default as ua } from './ua';
 export { default as ru } from './ru';
 
 export { default as createFromnow } from './createFromnow';
 
-export { default } from './Fromnow';
+export default Fromnow;
